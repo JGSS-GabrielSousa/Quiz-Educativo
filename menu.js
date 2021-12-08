05016dd0491bdd199fcd604f7784f2f4fd872916
JGSS-GabrielSousa <jgss.gabriel.sousa@gmail.com>
2021-12-08 19:45:59 -0300

    Initial

---
+++ menu.js
@@ -0,0 +1,9 @@
+function credits_screen() {
+    document.getElementById("main-menu").style.display = "none";
+    document.getElementById("credits").style.display = "block";
+}
+
+function back_main_menu() {
+    document.getElementById("main-menu").style.display = "block";
+    document.getElementById("credits").style.display = "none";
+}
