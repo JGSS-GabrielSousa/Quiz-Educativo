09bf0ba0229878118217a03096b77f1825faf7f6
Gabriel Sousa <jgss.gabriel.sousa@gmail.com>
2021-12-19 18:46:59 -0300

    Update geografia.js

--- data/geografia.js
+++ data/geografia.js
@@ -18,4 +18,22 @@ const geografia = [
         D: "Rússia",
         Correta: "D",
     },
+    {
+        pergunta: "Qual a montanha mais alta do Brasil?",
+        nome_imagem: "",
+        A: "Pico da Neblina",
+        B: "Pico da Bandeira",
+        C: "Monte Roraima",
+        D: "Pico das Agulhas Negras",
+        Correta: "A",
+    },
+    {
+        pergunta: "Qual a maior cidade do mundo?",
+        nome_imagem: "",
+        A: "São Paulo",
+        B: "Tóquio",
+        C: "Nova York",
+        D: "Xangai",
+        Correta: "B",
+    },
 ]
