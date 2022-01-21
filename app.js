3a9cc10d1a2d97796d7f8f78720c06a74d26a2a7
JGSS-GabrielSousa <jgss.gabriel.sousa@gmail.com>
2022-01-20 22:08:26 -0300

    nova tela de creditos

--- app.js
+++ app.js
@@ -16,16 +16,19 @@ function change_screen(screen){
         case "main-menu":
             document.getElementById("main-menu-btns").style.display = "grid";
             document.getElementById("credits").style.display = "none";
+            document.getElementById("title").innerText = "Quiz Educativo";
             break;
 
         case "credits":
             document.getElementById("main-menu-btns").style.display = "none";
-            document.getElementById("credits").style.display = "grid";
+            document.getElementById("credits").style.display = "inline-flex";
+            document.getElementById("title").innerText = "Créditos";
             break;
 
         case "game":
             document.getElementById("main-menu-btns").style.display = "none";
             document.getElementById("select-category").style.display = "flex";
+            document.getElementById("title").innerText = "Quiz Educativo";
             select_category_HTML();
             break;
 
